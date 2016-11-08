b8f9a1814bbf4c7e4c45b4120889b0e05c267b1e
grosinov <grosinov@gmail.com>
2016-11-08 14:43:45 -0300

    Juego Terminado

--- app/src/main/java/com/example/a41752347/planegame/clsJuego.java
+++ app/src/main/java/com/example/a41752347/planegame/clsJuego.java
@@ -1,9 +1,11 @@
 package com.example.a41752347.planegame;
 
+import android.graphics.Color;
 import android.util.Log;
 import android.view.MotionEvent;
 
 import org.cocos2d.actions.interval.ScaleBy;
+import org.cocos2d.layers.ColorLayer;
 import org.cocos2d.layers.Layer;
 import org.cocos2d.menus.MenuItemImage;
 import org.cocos2d.nodes.Director;
@@ -11,6 +13,7 @@ import org.cocos2d.nodes.Label;
 import org.cocos2d.nodes.Scene;
 import org.cocos2d.nodes.Sprite;
 import org.cocos2d.opengl.CCGLSurfaceView;
+import org.cocos2d.types.CCColor3B;
 import org.cocos2d.types.CCSize;
 
 import java.util.ArrayList;
@@ -29,6 +32,12 @@ public class clsJuego {
     int puntos = 0;
     int enemyTags = 1;
     int dispTags = 100000;
+    int tiempo = 6000;
+    Label Title;
+    Label Toca;
+    Label puntaje;
+    Label puntaje2;
+    CCColor3B black;
 
     ArrayList<AvionEnemigo> arrEnemigos;
     ArrayList<Disparo> arrDisparos;
@@ -38,6 +47,7 @@ public class clsJuego {
         avion = new Avion();
         arrEnemigos = new ArrayList<>();
         arrDisparos = new ArrayList<>();
+        black = new CCColor3B(Color.BLACK, Color.BLACK, Color.BLACK);
 
         vida = 3;
     }
@@ -47,7 +57,23 @@ public class clsJuego {
 
         TamañoPantalla = Director.sharedDirector().displaySize();
 
-        Director.sharedDirector().runWithScene(Escena());
+        Director.sharedDirector().runWithScene(EscenaInicio());
+    }
+
+    private Scene EscenaInicio(){
+        Scene escenaInicio;
+        escenaInicio = Scene.node();
+
+        CapaDeFondoInicio capfon;
+        capfon = new CapaDeFondoInicio();
+
+        CapaDeFrenteInicio capfren;
+        capfren = new CapaDeFrenteInicio();
+
+        escenaInicio.addChild(capfon, -10);
+        escenaInicio.addChild(capfren, 10);
+
+        return  escenaInicio;
     }
 
     private Scene Escena(){
@@ -82,6 +108,36 @@ public class clsJuego {
         return  escenaperdio;
     }
 
+    class CapaDeFondoInicio extends Layer{
+        public CapaDeFondoInicio(){
+            fondo = Sprite.sprite("fondo.png");
+            fondo.setPosition(TamañoPantalla.width / 2, TamañoPantalla.height / 2);
+            fondo.runAction(ScaleBy.action(0.01f, 14.0f, 10.0f));
+            super.addChild(fondo);
+        }
+    }
+
+    class CapaDeFrenteInicio extends Layer{
+        public CapaDeFrenteInicio(){
+            this.setIsTouchEnabled(true);
+            Title = Label.label("PlaneGame","Verdana" ,150);
+            Title.setColor(black);
+            Title.setPosition(TamañoPantalla.width / 2, TamañoPantalla.height / 2 + 200);
+            super.addChild(Title);
+
+            Toca = Label.label("Toca para jugar", "Verdana", 70);
+            Toca.setColor(black);
+            Toca.setPosition(TamañoPantalla.width / 2, TamañoPantalla.height / 2);
+            super.addChild(Toca);
+        }
+
+        @Override
+        public boolean ccTouchesBegan(MotionEvent event) {
+            Director.sharedDirector().replaceScene(Escena());
+            return true;
+        }
+    }
+
     class CapaDeFondo extends Layer {
         public CapaDeFondo() {
             fondo = Sprite.sprite("fondo.png");
@@ -105,15 +161,15 @@ public class clsJuego {
 
             vida1.setPosition(0 + vida1.getWidth() / 2, 0 + vida1.getHeight() / 2);
             vida1.runAction(ScaleBy.action(0.01f, 0.5f, 0.5f));
-            super.addChild(vida1, 1, 0);
+            super.addChild(vida1, 1, 200000);
 
             vida2.setPosition(vida1.getPositionX() + vida1.getWidth(), 0 + vida2.getHeight() / 2);
             vida2.runAction(ScaleBy.action(0.01f, 0.5f, 0.5f));
-            super.addChild(vida2, 1, 0);
+            super.addChild(vida2, 1, 200001);
 
             vida3.setPosition(vida2.getPositionX() + vida2.getWidth(), 0 + vida3.getHeight() / 2);
             vida3.runAction(ScaleBy.action(0.01f, 0.5f, 0.5f));
-            super.addChild(vida3, 1, 0);
+            super.addChild(vida3, 1, 200002);
 
             TimerTask TareaPonerEnemigos = new TimerTask() {
                 @Override
@@ -126,11 +182,49 @@ public class clsJuego {
             };
 
             Timer RelojEnemigos = new Timer();
-            RelojEnemigos.schedule(TareaPonerEnemigos, 0, 2000);
+            RelojEnemigos.schedule(TareaPonerEnemigos, 0, tiempo);
+            switch (puntos){
+                case 10:
+                    tiempo = 5500;
+                    break;
+                case 20:
+                    tiempo = 5000;
+                    break;
+                case 30:
+                    tiempo = 4500;
+                    break;
+                case 40:
+                    tiempo = 4000;
+                    break;
+                case 50:
+                    tiempo = 3500;
+                    break;
+                case 60:
+                    tiempo = 3000;
+                    break;
+                case 70:
+                    tiempo = 2500;
+                    break;
+                case 80:
+                    tiempo = 2000;
+                    break;
+                case 90:
+                    tiempo = 1500;
+                    break;
+                case 100:
+                    tiempo = 1000;
+                    break;
+            }
+            puntaje = Label.label("" + puntos,"Verdana" ,100);
 
             TimerTask TareaDetectarColisiones = new TimerTask() {
                 @Override
                 public void run() {
+                    puntaje.setString("" + puntos);
+                    puntaje.setColor(black);
+                    puntaje.setPosition(0 + puntaje.getWidth() + 20, TamañoPantalla.height - puntaje.getHeight() - 20);
+                    addChild(puntaje);
+
                     for (int i = 0; i < arrEnemigos.size(); i++) {
                         AvionEnemigo aven = arrEnemigos.get(i);
                         if(aven != null){
@@ -158,13 +252,17 @@ public class clsJuego {
                                 removeChild(aven.getAvionenemigo().getTag(), true);
                                 arrEnemigos.remove(i);
                                 vida--;
-                                if (vida == 2) {
-                                    removeChild(vida3.getTag(), true);
-                                } else if(vida == 1){
-                                    removeChild(vida2.getTag(), true);
-                                } else if(vida == 0){
-                                    removeChild(vida1.getTag(), true);
-                                    Director.sharedDirector().runWithScene(EscenaPerdio());
+                                switch (vida){
+                                    case 2:
+                                        removeChild(200002, true);
+                                        break;
+                                    case 1:
+                                        removeChild(200001, true);
+                                        break;
+                                    case 0:
+                                        removeChild(200000, true);
+                                        Director.sharedDirector().replaceScene(EscenaPerdio());
+                                        break;
                                 }
                             }
                         }
@@ -211,6 +309,21 @@ public class clsJuego {
 
         @Override
         public boolean ccTouchesEnded(MotionEvent event) {
+            float PosicionFinalY = avion.getAvion().getPositionY(), movimientovertical;
+            movimientovertical = TamañoPantalla.getHeight() - event.getY() - TamañoPantalla.getHeight() / 2;
+            int suavisadordemovimiento = 20;
+            movimientovertical = movimientovertical / suavisadordemovimiento;
+            if(event.getX() < TamañoPantalla.getWidth() / 2){
+                if(avion.getAvion().getPositionY() <= 0){
+                    PosicionFinalY = avion.getAvion().getPositionY() +1;
+                } else if (avion.getAvion().getPositionY() >= TamañoPantalla.getHeight()){
+                    PosicionFinalY = avion.getAvion().getPositionY() -1;
+                } else {
+                    PosicionFinalY = avion.getAvion().getPositionY() + movimientovertical;
+                }
+            }
+
+            avion.getAvion().setPosition(avion.getAvion().getPositionX(), PosicionFinalY);
             return true;
         }
     }
@@ -226,12 +339,28 @@ public class clsJuego {
 
     class CapaFrentePerdio extends Layer {
         public CapaFrentePerdio(){
-            Label labelScore = Label.label("Has Perdido!","Arial" ,20);
+            this.setIsTouchEnabled(true);
+            Title = Label.label("Has Perdido!","Verdana" ,150);
+            Title.setColor(black);
+            Title.setPosition(TamañoPantalla.width / 2, TamañoPantalla.height / 2 + 200);
+            super.addChild(Title);
+
+            Toca = Label.label("Toca para reintentar", "Verdana", 70);
+            Toca.setColor(black);
+            Toca.setPosition(TamañoPantalla.width / 2, TamañoPantalla.height / 2);
+            super.addChild(Toca);
+
+            puntaje2 = Label.label("Puntaje: " + puntos, "Verdana", 70);
+            puntaje2.setColor(black);
+            puntaje2.setPosition(TamañoPantalla.width / 2, TamañoPantalla.height / 2 - 200);
+            addChild(puntaje2);
+
+            puntos = 0;
         }
 
         @Override
         public boolean ccTouchesBegan(MotionEvent event) {
-            Director.sharedDirector().runWithScene(Escena());
+            Director.sharedDirector().replaceScene(Escena());
             vida = 3;
             return true;
         }
